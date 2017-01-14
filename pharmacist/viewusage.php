efb76ec4382b920feba5d9fb1cd8fe75eb41d435
udarshika <udarshika94@gmail.com>
2017-01-14 13:54:26 +0530

    view usage

--- pharmacist/viewusage.php
+++ pharmacist/viewusage.php
@@ -1,97 +1,29 @@
 <html>
 <head>
-	 <link rel="stylesheet" href="css/stockstyles.css" />
-         <script src="js/jquery-2.0.0.js"></script>
-        <script src="js/moment.min.js"></script>
-        
-        <style>
-            td {text-align: center;}
-        </style>
-        <script>
-            $(function(){
-               loadusage(); 
-            });
-            
-            function loadusage(){
-                $.ajax({
-                    url:"rest1.php",
-                    async:true,
-                    type:"GET",
-                    data : {
-                        action: $('#action').val(),
-                        medtype : $('#medtype').val(),
-                        search : $('#search').val(),
-                        period: $('#period').val()
-                   },
-                })
-                .done(function(data){
-                    var records = JSON.parse(data);
-                    var tbl_bdy =
-                    document.getElementById("table-body");
-                    tbl_bdy.innerHTML = '';
-                    /*
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Batch Number</th>
-                        <th>Supplier</th>
-                        <th>Quantity</th>
-                        <th>Entry Date</th>
-                        <th>Production Date</th>
-                        <th>Expire Date</th>
-                        <th>Buying Price</th>
-                        <th>Selling Price</th>
-                    */
-                    for (var i=0; i < records.length; i++){
-                        var row = "<tr>";
-                        row +="<td>" + records[i].date + "</td>";
-                        row +="<td>" + records[i].medicine_name + "</td>";
-                        row +="<td>" + records[i].category + "</td>";
-                        row +="<td>" + records[i].batch_no + "</td>";
-                        row +="<td>" + records[i].supplier + "</td>";
-                        row +="<td>" + records[i].quantity + "</td>";
-                        if(records[i].buying_price === undefined)
-                            row +="<td>-</td>";
-                        else
-                            row +="<td>" + records[i].buying_price + "</td>";
-                        if(records[i].selling_price === undefined)
-                            row +="<td>-</td>";
-                        else
-                        row +="<td>" + records[i].selling_price+ "</td></tr>";
-                        tbl_bdy.innerHTML += row;
-                    }
-                    })
-                }
-            
-            function getStartDate() {
-                var limit = $('#period').val();
-                var d = new Date();
-                switch(limit) {                    
-                    case '1': {
-                        d.setFullYear(d.getYear()-1);
-                    }
-                    break;
-                    case '2': {
-                        d.setMonth(d.getMonth()-1);
-                    } 
-                    break;
-                    case '3': {
-                        d.setDate(d.getDate()-7);
-                    }
-                    break;
-                    case '4':{
-                        d.setDate(d.getDate()-1);
-                    }
-                    break;
-                    default: {
-                        d = 'null';
-                    }
-                }
-                return d;
-            }
-        </script>
-            
     <?php require('../includes/_header.php'); ?>
-	
+    <script src="../public/js/sort.js"></script>
+	<link rel="stylesheet" href="css/stockstyles.css"/>
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="css/selectize.css"/>
+    <script src="js/jquery-2.0.0.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/selectize.min.js"></script>
+    
+    <style>
+        .selectize-control {
+            width: 250px;
+        }
+    </style>
+    <script>
+        $(function() {
+            $('select').selectize({
+                persist: true,
+                createOnBlur: true,
+            });    
+        });
+        
+    </script>
+        
 </head>
 
 <body>
@@ -99,67 +31,58 @@
     <?php require_once("../includes/navigation.php") ?>
     
     <!--content goes here -->
-    <div class="customer_template_container" style="z-index: -1;">
+    <div class="customer_template_container">
         
-         <h2>View Usage</h2>
+        <h2>View Usage</h2>
 
+        <h2 style="text-align:center;">View Usage</h2>
+        <div class="content" style="margin-top:10px; padding-right:40px; padding-left:40px;">
+            <table>
+                <td>
+                Search <select id="drug">
+            <?php
+            include '../database/dbconnect.php';
+            $sql = "SELECT * FROM drug";
+            $result = mysqli_query($mysqli, $sql);
+
+            while (($row = mysqli_fetch_assoc($result)) != null) {
+                echo "<option value='" . $row['id'] . "'>" . $row['generic_name'] . "</option>";
+            }
+            ?>
+                    </select>
+                </td><td>
+                Duration <select id="duration">
+                                <option value="1">One day</option>
+                                <option value="7">One week</option>
+                                <option value="30">One month</option>
+                                <option value="365">One year</option>
+
+                            </select>
+            </td>
+            </table>
+        </div>
+<!--
         <div class="top-bar">
-           <!-- <div class="left-float" style="padding-top:5px;">
-                   Type
-                <select name="action" id="action" onchange='loadusage()'>
-                    <option value="1">Bought</option>
-                    <option value="2">Sold</option>
-                </select>
-            </div>-->
             <div class="left-float" style="padding-top:5px;">
                 Search By:
-                <select name="medtype" id="medtype" onchange='loadusage()'>
-                  <option value="1">Medicine Name</option>
-                  <option value="2">Batch Number</option>
-                  <option value="3">Category</option>
+                <select name="medtype" id="medtype">
+                    <option value="1">Medicine Name</option>
+                    <option value="2">Batch Number</option>
+                    <option value="3">Category</option>
                 </select>
-                <input type="text" name="search" id="search" placeholder="Search" oninput='loadusage()'/>
-            </div>
-            <div class="left-float" style="padding-top:5px;">
-               Period
-                <select name="period" id="period" onchange="loadusage()">
-                    <option value="1">One Year</option>
-                    <option value="2">One Month</option>
-                    <option value="3">One Week</option>
-                    <option value="4">One Day</option>
-                </select>
+                <input type="text" name="search" id="search" placeholder="Search" oninput='loadStock()'/>
             </div>
             <div class="left-float">
-                <span class="icon"><img src="Zoom-icon.png" height="40" width="40" /></span>
+                <span class="icon" onclick="loadStock()"><img src="img/Zoom-icon.png" height="40" width="40" /></span>
             </div>
             <div>
                 
             </div>
         </div>
-        
-        <div class="content">
-            <table border="0">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Batch Number</th>
-                        <th>Supplier</th>
-                        <th>Quantity</th>
-                        <th>
-                            <select name="action" id="action" onchange='loadusage()' style='background-color:rgb(214,244,185);'>
-                                <option value="1">Bought</option>
-                                <option value="2">Sold</option>
-                            </select>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody id="table-body">
-                </tbody>
-            </table>
+-->
+
+            
         </div>
-        
         <?php // echo $content; ?>
     </div>		
 	
