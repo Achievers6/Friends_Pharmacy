c55446ba63e3713b12fee1ad8b10b8c6231c8a71
udarshika <udarshika94@gmail.com>
2017-01-14 10:59:05 +0530

    Var name change

--- customer/rest3.php
+++ customer/rest3.php
@@ -6,8 +6,9 @@ if (isset($_GET['nic'])) {
 }
 $type = $_GET['type'];
 
-$conn = mysqli_connect("localhost", "root", "", "friends_pharmacy");
-if (!$conn) {
+//$conn = mysqli_connect("localhost", "root", "", "friends_pharmacy");
+include '../database/dbconnect.php';
+if (!$mysqli) {
     echo "Error";
 }
 
@@ -33,7 +34,7 @@ switch($type) {
     break;
     case 'details': {
         $sql = "SELECT * FROM reminder_table WHERE nic = '$nic'";
-        $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($mysqli, $sql);
         $res_data = array();
         while ($row = mysqli_fetch_assoc($result)) {
             array_push($res_data, $row);
@@ -44,12 +45,12 @@ switch($type) {
     case 'changeActive': {
         $active = $_GET['active'];
         $sql = "UPDATE customer SET active=$active WHERE nic='$nic'";
-        mysqli_query($conn, $sql);
+        mysqli_query($mysqli, $sql);
     }
     break;
     case '2': {
         $sql = "SELECT * FROM stock s, drug d WHERE s.medicine_id=d.medicine_id AND  d.medicine_name LIKE '$search%'";
-        $re10ult = mysqli_query($conn, $sql);
+        $re10ult = mysqli_query($mysqli, $sql);
         $res_data = array();
         while($row = mysqli_fetch_assoc($result)) {
             
@@ -60,7 +61,7 @@ switch($type) {
     break;
     case '3': {
         $sql = "SELECT * FROM stock s, drug d WHERE s.medicine_id=d.medicine_id AND  d.batch_no LIKE '$search%'";
-        $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($mysqli, $sql);
         $res_data = array();
         while($row = mysqli_fetch_assoc($result)) {
             
