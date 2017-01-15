42a971635eb105cd6021303aa959c789c00d215b
Mithsen <MithsenDeSilva@gmai.com>
2017-01-15 06:47:28 +0530

    send order massage

---
+++ msg/test.php
@@ -0,0 +1,7 @@
+<?php
+include 'example.php';
+$text = new text();
+$no='94711446323';
+$msg = 'this is from nexmo';
+$text->msg($no,$msg);
+?>
--- pharmacist/confiredOrder.php
+++ pharmacist/confiredOrder.php
@@ -7,8 +7,43 @@ $orderController = new orderController();
 if (isset($_GET["confirmed"])) {
     $orderController->removeOrder($_GET["confirmed"]);
     $orderTable = $orderController->orderConfirmTable("confirmed");
-    $content = $orderTable;
-} else {
+    $content = "<div style='float:left'>
+        <h2 style='text-align:center; position:relative; left:100px;'>Confirmed orders List<h2>
+        <table style='position:relative; left:30px;'>
+        <tr>
+            <td><div style=' font-size: 1.25em;'>Email</div></td>
+            <td>
+            <div class='container-1'>
+            <span class='icon'><i class='fa fa-search'></i></span>
+            <input type='text' name='email' id='email' placeholder='search' oninput='loadcustomers()'/>
+            </td>
+            </div>
+            <tr>
+            </table>
+            <div id='List' style='position:relative; left:120px; top:40px;'>$orderTable</div> 
+            </div>";
+} 
+else if(isset($_GET["smsemail"])) {
+    $msg = "your order is ready.Order number is: ".$_GET['orderNo'];
+    //$orderController->sendSms($_GET["smsemail"],$msg);
+    $orderTable = $orderController->orderConfirmTable("confirmed");
+    $content = "<div style='float:left'>
+        <h2 style='text-align:center; position:relative; left:100px;'>Confirmed orders List<h2>
+        <table style='position:relative; left:30px;'>
+        <tr>
+            <td><div style=' font-size: 1.25em;'>Email</div></td>
+            <td>
+            <div class='container-1'>
+            <span class='icon'><i class='fa fa-search'></i></span>
+            <input type='text' name='email' id='email' placeholder='search' oninput='loadcustomers()'/>
+            </td>
+            </div>
+            <tr>
+            </table>
+            <div id='List' style='position:relative; left:120px; top:40px;'>$orderTable</div> 
+            </div>";
+}
+else {
     $orderTable = $orderController->orderConfirmTable("confirmed");
     $content = "<div style='float:left'>
         <h2 style='text-align:center; position:relative; left:100px;'>Confirmed orders List<h2>
