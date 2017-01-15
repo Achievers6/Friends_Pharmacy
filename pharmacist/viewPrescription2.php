75778400344e661fce3fe6981a53339f9cde3087
Mithsen <MithsenDeSilva@gmai.com>
2017-01-15 16:52:14 +0530

    add pres

--- pharmacist/viewPrescription2.php
+++ pharmacist/viewPrescription2.php
@@ -1,105 +1,131 @@
-<?php
-	$n = "Sandu";
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="../public/css/customer/prescription.css">
+<style>
+#myImg {
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+}
 
-	<script>
-		function add()
-		{
-			var medi_name = "piriton";
-			var quantity = 10;
-			var dosage = 5;
-			var unitprice = 3;
-			var totalPrice = 30;
+#myImg:hover {opacity: 0.7;}
 
-			var table = document.getElementById("tbl");
-			var row = table.insertRow(-1);
-			var medicineCell = row.insertCell(0);
-	        var quantityCell = row.insertCell(1);
-	        var dosageCell = row.insertCell(2);
-	        var unitPriceCell = row.insertCell(3);
-	        var totalPriceCell = row.insertCell(4);
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
 
-	        var totalPrice = parseInt(unitprice) * parseInt(quantity);
+/* Modal Content (image) */
+.modal-content {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+}
 
-	        medicineCell.innerHTML = medi_name;
-	        quantityCell.innerHTML = quantity;
-	        dosageCell.innerHTML = dosage;
-	        unitPriceCell.innerHTML = unitprice;
-	        totalPriceCell.innerHTML = totalPrice;
+/* Caption of Modal Image */
+#caption {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    text-align: center;
+    color: #ccc;
+    padding: 10px 0;
+    height: 150px;
+}
 
-	        
-		}
-	</script>
+/* Add Animation */
+.modal-content, #caption {    
+    -webkit-animation-name: zoom;
+    -webkit-animation-duration: 0.6s;
+    animation-name: zoom;
+    animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+    from {-webkit-transform:scale(0)} 
+    to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+    from {transform:scale(0)} 
+    to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+    position: absolute;
+    top: 15px;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+    color: #bbb;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+    .modal-content {
+        width: 100%;
+    }
+}
+</style>
 </head>
 <body>
-<?php
 
-	include '../database/dbconnect.php';
+<h2>Image Modal</h2>
+<p>In this example, we use CSS to create a modal (dialog box) that is hidden by default.</p>
+<p>We use JavaScript to trigger the modal and to display the current image inside the modal when it is clicked on. Also note that we use the value from the image's "alt" attribute as an image caption text inside the modal.</p>
 
-	$content = "";
+<img id="myImg" src="img_fjords.jpg" alt="Trolltunga, Norway" width="300" height="200">
 
-	$id = $_GET['prescription_id'];
-
-	$sql = "SELECT * FROM Prescription WHERE prescription_id='$id'";
-	$result = mysqli_query($mysqli, $sql);
-	while ($row = mysqli_fetch_array($result)) 
-	{
-		echo "
-		<h2> Prescription ID - ".$id." </h2>
-		<div class='image'>
-			<img src='../customer/uploads/".$row['image']."'>
-		</div>
-		";
-	}
-
-
-?>
-<div class="mid">
-	<form >
-		Search Medicine:		
-		<select id='medicine_list'>
-            <?php
-                include '../database/dbconnect.php';
-                //$conn = mysqli_connect("localhost", "root", "", "friends_pharmacy");
-                $sql = "SELECT * FROM drug_price";
-                $result = mysqli_query($mysqli, $sql);
-                while(($row = mysqli_fetch_assoc($result)) != null) {
-                    echo "<option value='" . $row['id'] . "'>" . $row['medicine_name'] . " " . $row['dosage'] . "</option>";
-                }
-            ?>
-        </select>
-
-        <script>
-        $('#medicine_list').selectize({
-            persist: false,
-            createOnBlur: true,
-        });
-        </script><br><br>
-        Quantity : <input type="number" name="quantity" id="quantity" > <br><br>
-        <input type="button" onclick="add()" value="Add"> 
-        <br><br>
-        <input type="button" onclick="finish()" value="Finish">
-
-	</form>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="img01">
+  <div id="caption"></div>
 </div>
 
-<div class="right" id="div_to_print">
-	<table border=1 id="tbl" style="width:100%;border-collapse: collapse;font-weight:100">
-        <tr>        
-            <th>Medicine Name</th>
-            <th>Quantity</th>
-            <th>Dosage</th>
-            <th>Unit Price</th>
-            <th>Total Price</th>
-        </tr>
-    </table>
-</div>
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+</script>
 
 </body>
 </html>
